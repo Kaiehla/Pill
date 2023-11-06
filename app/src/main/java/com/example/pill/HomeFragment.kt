8f475d7627ebbfaae01ac5aeec7edc80922ffd71
Kai <espiritu.kaiehla@gmail.com>
2023-11-06 23:43:14 +0800

    add recyclerview in home fragment

--- app/src/main/java/com/example/pill/HomeFragment.kt
+++ app/src/main/java/com/example/pill/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +20,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
+    //recycler view
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var dataList: ArrayList<DataClass>
+    lateinit var imageList: Array<Int>
+    lateinit var titleList: Array<String>
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -27,14 +37,59 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val viewFragment = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        //recycler view
+        imageList = arrayOf(
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill,
+            R.drawable.icon_pill
+        )
+
+        titleList = arrayOf(
+            "Trixera",
+            "Avene",
+            "Hyabak",
+            "Biogesic",
+            "Allerta",
+            "Zykast",
+            "Montelukast",
+            "Vitamin A",
+            "Vitamin B",
+            "Vitamin C",
+            "Vitamin D",
+            "Vitamin E",
+        )
+
+        recyclerView = viewFragment.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.setHasFixedSize(true)
+
+        dataList = arrayListOf<DataClass>()
+        getData()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return viewFragment
     }
 
     companion object {
@@ -55,5 +110,15 @@ class HomeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    //recycler view
+    private fun getData(){
+        for (i in imageList.indices){
+            val dataClass = DataClass(imageList[i], titleList[i])
+            dataList.add(dataClass)
+        }
+
+        recyclerView.adapter = AdapterClass(dataList)
     }
 }
