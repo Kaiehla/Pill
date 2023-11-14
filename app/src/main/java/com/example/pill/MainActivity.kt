a2e5e130215255a7aed751a73822b469dd96bd1f
Kai <espiritu.kaiehla@gmail.com>
2023-11-14 10:15:03 +0800

    fixed bottom nav with color active

--- app/src/main/java/com/example/pill/MainActivity.kt
+++ app/src/main/java/com/example/pill/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         //Splash screen
         Handler().postDelayed({
-            val intent = Intent(this, Login::class.java)
+            val intent = Intent(this, Home::class.java)
             startActivity(intent)
         }, 2000)
     }
